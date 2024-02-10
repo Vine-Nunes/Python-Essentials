5c0023c01f4b0adf6285615a86b7549724415145
Vine-Nunes <157760373+Vine-Nunes@users.noreply.github.com>
2024-02-09 21:09:53 -0300

    Update Módulo4.py

--- Módulo4.py
+++ Módulo4.py
@@ -1,40 +1,13 @@
-year = int(input("tell me any year: "))
-month = int(input("select a month from 1 to 12: "))
-day = int(input("chosse a day: "))
-if year > 2024:
-    print(None)
-    break
-elif month > 12:
-    print(None) 
-    break   
-if year%4==0 and month==2:
-    
-    if day > 29:
-        print(None)
+def num(primo):
+    if primo%2 != 0 or primo==2 and primo>1:
+           if primo%3 != 0 or primo==3:
+              print(True,primo)
+           else:
+              print(False,primo)
     else:
-        print(day, month, year)
+       print(False)
 
-elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
-   
-    if day > 31:
-        print(None)
-    else:
-        print(day, month, year)
-
-elif month==2:
-    
-    if day > 28:
-        print(None)
-    else:
-        print(day, month, year)
-
-elif month==4 or month==6 or month==9 or month==11: 
-    
-    if day > 30:
-        print(None)
-    else:
-        print(day, month, year)
-else:
-    None
-
-
+for i in range(1,20):
+    if num(i+1):
+        print(i+1)
+        
